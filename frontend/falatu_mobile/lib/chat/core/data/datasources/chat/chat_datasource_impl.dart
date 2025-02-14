404899cb243d4edbcd8ce338735779c6359aadeb
Lucas Monteiro <lucasmonteirob1@outlook.com>
2025-02-14 10:42:43 -0300

    chore: dart fix

--- frontend/falatu_mobile/lib/chat/core/data/datasources/chat/chat_datasource_impl.dart
+++ frontend/falatu_mobile/lib/chat/core/data/datasources/chat/chat_datasource_impl.dart
@@ -1,5 +1,4 @@
 import "dart:async";
-import "dart:io";
 
 import "package:falatu_mobile/chat/core/data/datasources/chat/chat_datasource.dart";
 import "package:falatu_mobile/chat/core/data/models/chat/chat_model.dart";
@@ -10,7 +9,6 @@ import "package:falatu_mobile/commons/core/data/services/socket_io_service/socke
 import "package:falatu_mobile/commons/core/domain/entities/base_error.dart";
 import "package:falatu_mobile/commons/core/domain/entities/result_wrapper.dart";
 import "package:falatu_mobile/commons/utils/helpers/url_helpers.dart";
-import "package:flutter/cupertino.dart";
 
 class ChatDatasourceImpl extends ChatDatasource {
   final SharedPreferencesService _preferences;
